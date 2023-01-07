1d083eb445b78993c11784b569f98c23ce22c5b8
MissUwuieTime <87099578+MissUwuieTime@users.noreply.github.com>
2023-01-07 15:56:20 -0600

    Add version display dynamically in About page

--- src/main/java/views/pages/AboutPage.java
+++ src/main/java/views/pages/AboutPage.java
@@ -2,9 +2,15 @@ package views.pages;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import viewmodels.pages.AboutViewModel;
 
 import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Singleton
 public class AboutPage extends Page {
@@ -15,10 +21,13 @@ public class AboutPage extends Page {
     private JTextArea legalTextArea;
 
     @Inject
-    public AboutPage(AboutViewModel aboutViewModel) {
+    public AboutPage(AboutViewModel aboutViewModel) throws IOException, XmlPullParserException {
         super();
         viewModel = aboutViewModel;
         wordmarkLabel.setIcon(viewModel.getWordmarkImageIcon());
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
+        versionNumberLabel.setText(model.getVersion());
         legalTextArea.setFocusable(false);
         add(rootPanel);
     }
